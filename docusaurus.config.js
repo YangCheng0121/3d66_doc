017e586a48110c803d2bdb511265d046c6ce75d7
YangCheng <yang759126596@163.com>
2023-01-13 14:10:36 +0800

    fix: 修改为zh-hans

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,8 +23,8 @@ const config = {
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'zh',
-    locales: ['zh'],
+    defaultLocale: 'zh-hans',
+    locales: ['zh-hans'],
   },
 
   presets: [
